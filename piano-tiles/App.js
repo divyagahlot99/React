4f60c55395260e72bf64460f3cfd87888f315e08
Divya Gahlot <gahlotdivya1019@gmail.com>
2021-05-01 03:07:30 +0530

    Create App.js

---
+++ piano-tiles/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Tiles from './components/Tiles'
+
+function App() {
+	return (
+		<div className="App">
+			PIANO
+			<Tiles/>
+		</div>
+	);
+}
+
+export default App;
